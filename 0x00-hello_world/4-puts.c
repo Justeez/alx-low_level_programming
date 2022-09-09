656a6ba6ed94ed909adfc075b3b2509b0a8afb1a
Justeez <okwara_justice@yahoo.com>
2022-09-08 18:37:45 -0700

    "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+#include <studio.h>
+
+int main(void)
+
+{
+
+	    
+
+	  
+	 char str[20] = "Programming is like building a multilingual puzzle";
+
+		    
+
+	    puts(str);
+	    return 0;
+
+}
