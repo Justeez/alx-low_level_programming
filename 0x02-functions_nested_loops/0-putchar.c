7ebc7be1fd13b2e6e9996fdd86517471b00b9e14
Justeez <okwara_justice@yahoo.com>
2022-09-13 15:24:58 -0700

    Printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *Main - Program that prints _putchar.
+ *Description: this code prints _putchar
+ *Return: always return 0
+ */
+
+int main(void)
+{
+	_putchar('M');
+	_putchar('A');
+	_putchar('I');
+	_putchar('N');
+	_putchar('\n');
+	return (0);
+}
