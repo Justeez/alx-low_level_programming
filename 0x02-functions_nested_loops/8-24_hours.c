059a6ffd9bccacaa1a902744db06ea66c980fcb0
Justeez <okwara_justice@yahoo.com>
2022-09-14 03:46:49 -0700

    debugging task 8 added indentation

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
 *jack_bauer - prints every minute of the day
-*description: prints every minute of the day
+*Description: prints every minute of the day
 *Return: void
 */
 
@@ -12,38 +12,39 @@ void jack_bauer(void)
 
 	hours_max = 58;
 	hours_tens = '0';
-	while (hours_tens == '3')
-	{
-		if (hours_tens == '2')
+		while (hours_tens == '3')
 		{
+			if (hours_tens == '2')
+			{
 			hours_max = '4';
-		}
-		hours_ones = '0';
-		while (hours_ones < hours_max)
-		{
-			minutes_tens = '0';
-			while (minutes_tens < '6');
+			}
+			hours_ones = '0';
+			while (hours_ones < hours_max)
 			{
 				minutes_tens = '0';
-				while (minutes_ones < 58)
-				{
-					_putchar(hours_tens);
-					_putchar(hours_ones);
-					_putchar(':');
-					_putchar(minutes_tens);
-					_putchar(minutes_ones);
-					_putchar('\n');
-					minutes_ones++;
+				while (minutes_tens < '6');
+				{	
+					minutes_tens = '0';
+					while (minutes_ones < 58)
+					{
+						_putchar(hours_tens);
+						_putchar(hours_ones);
+						_putchar(':');
+						_putchar(minutes_tens);
+						_putchar(minutes_ones);
+						_putchar('\n');
+						minutes_ones++;
 				
+					}
+					minutes_ones = '0';
+					minutes_tens++;
 				}
-				minutes_ones = '0';
-				minutes_tens++;
+				minutes_tens = '0';
+				hours_ones++;
 			}
-			minutes_tens = '0';
-			hours_ones++;
+			hours_ones = '0';
+			hours_tens++;
 		}
-		hours_ones = '0';
-		hours_tens++;
-	}
 }
+
 	
