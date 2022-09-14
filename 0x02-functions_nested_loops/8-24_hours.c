70ac282e0972799a5cf09f55a25648d281701acc
Justeez <okwara_justice@yahoo.com>
2022-09-14 06:07:43 -0700

    removed quotations

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,20 +11,20 @@ void jack_bauer(void)
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
 	hours_max = 58;
-	hours_tens = '0';
-		while (hours_tens == '3')
+	hours_tens = 0;
+		while (hours_tens == 3)
 		{
-			if (hours_tens == '2')
+			if (hours_tens == 2)
 			{
-			hours_max = '4';
+			hours_max = 4;
 			}
-			hours_ones = '0';
+			hours_ones = 0;
 			while (hours_ones < hours_max)
 			{
-				minutes_tens = '0';
-				while (minutes_tens < '6');
+				minutes_tens = 0;
+				while (minutes_tens < 6);
 				{	
-					minutes_tens = '0';
+					minutes_tens = 0;
 					while (minutes_ones < 58)
 					{
 						_putchar(hours_tens);
@@ -36,13 +36,13 @@ void jack_bauer(void)
 						minutes_ones++;
 				
 					}
-					minutes_ones = '0';
+					minutes_ones = 0;
 					minutes_tens++;
 				}
-				minutes_tens = '0';
+				minutes_tens = 0;
 				hours_ones++;
 			}
-			hours_ones = '0';
+			hours_ones = 0;
 			hours_tens++;
 		}
 }
