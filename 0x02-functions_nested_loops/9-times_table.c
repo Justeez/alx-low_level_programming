6af135f6d52eb6bb5d0d6e612c87c4ee340e2374
Justeez <okwara_justice@yahoo.com>
2022-09-14 04:15:30 -0700

    indented task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,6 +13,7 @@ void times_table(void)
 	int row, column, product, tens, ones;
 
 	for (row = 0; row <= 9; row++)
+	{
 
 		for (column = 0; column <= 9; column++)
 		{
@@ -38,6 +39,6 @@ void times_table(void)
 				_putchar(ones + '0');
 			}
 		}
-		_putchar('\n');
-	}
+	}	_putchar('\n');
 }
+
