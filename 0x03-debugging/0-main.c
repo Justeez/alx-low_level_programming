b93444da965c6c85c0d13fd2c7d13cb79299111a
Justeez <okwara_justice@yahoo.com>
2022-09-15 13:00:13 -0700

    removed trailing spaces

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 int main(void)
 {
 	int i;
@@ -13,5 +12,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
